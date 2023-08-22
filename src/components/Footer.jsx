1dc54d433676916170bebad04dcc93a4cfdb3511
IGIRANEZA Josue <igiranezaj28@gmail.com>
2023-08-23 01:23:59 +0300

    Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,8 +2,38 @@ import React from 'react'
 
 const Footer = () => {
    return (
-      <footer>
-         Footer
+      <footer className="flex items-start justify-around text-white/10">
+         <div>
+            <p>FAQ</p>
+            <p>Investor relations</p>
+            <p>Ways to watch</p>
+            <p>Corporate information</p>
+            <p>Only on DreamCloud</p>
+         </div>
+
+         <div>
+            <p>Help Center</p>
+            <p>Jobs</p>
+            <p>Terms Of Use</p>
+            <p>Contact Us</p>
+         </div>
+
+         <div>
+            <p>Account</p>
+            <p>Privacy</p>
+            <p>Speed Test</p>
+         </div>
+
+         <div>
+            <p>Media center</p>
+            <p>Cookie Prefrences</p>
+            <p>Legal Notices</p>
+
+            <span className=" rounded-lg px-6 py-2 bg-primaryPurple cursor-pointer text-sm">
+               <p>Get Started</p>
+            </span>
+         </div>
+
       </footer>
    )
 }
