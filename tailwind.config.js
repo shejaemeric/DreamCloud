9abf98ffc5c7ba9c5ad7c2b10fd7abfcc765c82c
IGIRANEZA Josue <igiranezaj28@gmail.com>
2023-08-27 18:40:48 +0300

    Added tailwind colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primaryPurple: "#522897"
+      }
+    },
   },
   plugins: [],
 }
