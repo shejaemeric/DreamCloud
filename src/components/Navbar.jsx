a7f85ad8bed0636e99440ec024de6f86fe8aea50
IGIRANEZA Josue <igiranezaj28@gmail.com>
2023-08-28 12:49:23 +0300

    changed it back

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,13 @@
-/* eslint-disable react/prop-types */
 import React from "react";
 import logo from "../assets/DreamCloud Logo 1 (1).svg";
 import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 import { auth } from "../base";
 import { showToast } from "../utils";
+import { useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
 
-const Navbar = (props) => {
+const Navbar = () => {
+  const navigate = useNavigate();
   const [user, setUser] = useState(JSON.parse(localStorage.getItem("user")));
 
   const login = async () => {
@@ -41,31 +42,13 @@ const Navbar = (props) => {
             <a href="#">Podcasts</a>
           </div>
         </div>
-        {props.includeSearch && (
-          <div>
-            <input
-              className=" mr-6 w-[230px] py-3 px-4 bg-transparent border-2 rounded-full outline-none"
-              type="text"
-              placeholder="Search..."
-              onChange={props.handleInputChange}
-            />
-            <button
-              className=" bg-primaryPurple w-[64px] h-[64px] rounded-full "
-              onClick={props.search}
-            >
-              <i className="fa-solid fa-magnifying-glass text-white text-[18px]"></i>
-            </button>
-          </div>
-        )}
 
         <div className="navButtons flex items-center gap-8 font-medium">
           <span className=" rounded-lg px-6 py-2 border-white border-2 cursor-pointer text-[14px]">
             {!user ? (
               <p onClick={login}>Login</p>
             ) : (
-              <span className=" flex items-center justify-center w-[40px] h-[40px] rounded-full bg-primaryPurple">
-                <i class="fa-regular fa-user"></i>
-              </span>
+              <p>{`Welcome ${getUserName()}`}</p>
             )}
           </span>
           {/* <span className=" rounded-lg px-6 py-2 bg-primaryPurple cursor-pointer text-[14px]">
